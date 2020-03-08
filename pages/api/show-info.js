e09e4104ff5fc5d73c7b59555732ccbcd5b7dc6e
Dan Amyot <amyot.dan@gmail.com>
2020-03-08 16:17:08 -0400

    feat: show info endpoint

--- pages/api/show-info.js
+++ pages/api/show-info.js
@@ -8,8 +8,8 @@ const TVDB_TOKEN =
   "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODQwNTc1NDcsImlkIjoiRGFuIEFteW90Iiwib3JpZ19pYXQiOjE1ODM0NTI3NDcsInVzZXJpZCI6MjI2MjU5MywidXNlcm5hbWUiOiJkYW5hbXlvdCJ9.GAybuWqSaNSBpm2gYNWVwdp9NKorEsyEmhTg6ypieYs1sY4eR-k6m8L1-wgFxqIuDDn_zx92lSnjvsF06RKJHGwtuo-dcymTIRE6jV6V5ZAJJRISFjR41pf6Iy3oQqG1TVnossjPnz30LqV0SdbF63TC4vja_xGBvSwlmMXMWr1xuwdYYIuyaEGa1irY6jILOQnbuBGOIRfz71TZ612ybC7BOUbgPUjYh6dCJ3lwRaX00666Ny9_GV1M02Rfb9lzOblOeTg1i62lIZhLJzjlUITbl1foeK6OKQl_n9VMaSdI0jr85xO-sMXCBkt1p2DqGOI0dybO_cpjWlibqKU7qQ";
 
 export default async (req, res) => {
-  const trendingShows = await (
-    await fetch(`${BASE_TRAKT_URL}/shows/popular?limit=${req.query.limit}`, {
+  const fullShowInfo = await (
+    await fetch(`${BASE_TRAKT_URL}/shows/${req.query.id}?extended=full`, {
       headers: {
         "Content-Type": "application/json",
         "trakt-api-version": "2",
@@ -18,28 +18,33 @@ export default async (req, res) => {
     })
   ).json();
 
-  const showsWithImage = await Promise.all(
-    trendingShows.map(async showInfo => {
-      let newShow = {
-        ...showInfo
-      };
+  fullShowInfo.ids.tvdb;
 
-      const showImageData = await (
-        await fetch(
-          `${BASE_TVDB_URL}/series/${showInfo.ids.tvdb}/images/query?keyType=poster`,
-          { headers: { Authorization: "Bearer " + TVDB_TOKEN } }
-        )
-      ).json();
+  const [showPosters, showFanart, showSeries] = await Promise.all([
+    fetch(
+      `${BASE_TVDB_URL}/series/${fullShowInfo.ids.tvdb}/images/query?keyType=poster`,
+      { headers: { Authorization: "Bearer " + TVDB_TOKEN } }
+    ),
+    fetch(
+      `${BASE_TVDB_URL}/series/${fullShowInfo.ids.tvdb}/images/query?keyType=fanart`,
+      { headers: { Authorization: "Bearer " + TVDB_TOKEN } }
+    ),
+    fetch(
+      `${BASE_TVDB_URL}/series/${fullShowInfo.ids.tvdb}/images/query?keyType=series`,
+      { headers: { Authorization: "Bearer " + TVDB_TOKEN } }
+    )
+  ]);
 
-      const randShowImage = Math.floor(
-        Math.random() * showImageData.data.length
-      );
+  const imageData = {
+    posters: await showPosters.json(),
+    fanart: await showFanart.json(),
+    series: await showSeries.json()
+  };
 
-      newShow.imageData = showImageData.data[randShowImage];
+  const showWithImage = {
+    ...fullShowInfo,
+    imageData
+  };
 
-      return newShow;
-    })
-  );
-
-  res.status(200).json(showsWithImage);
+  res.status(200).json(showWithImage);
 };
--- todo.md
+++ todo.md
@@ -1,3 +1,5 @@
 ## Questions
 
 - [ ] What does `exportPathMap` do?
+- [ ] Is the first page the only one that's truly rendered server-side?
+- [ ] How do I set up CORS?
