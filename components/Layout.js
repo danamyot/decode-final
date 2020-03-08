03e5f9d8063081637bce9a712d4631d6979f10eb
Dan Amyot <amyot.dan@gmail.com>
2020-03-08 13:52:31 -0400

    chore: update page title

--- components/Layout.js
+++ components/Layout.js
@@ -42,7 +42,9 @@ class Layout extends React.Component {
         }`}
       >
         <Head>
-          <title>Next.js Starter</title>
+          <title>
+            Trakr.tv - Track and follow all of your favorite TV shows
+          </title>
           <meta name="description" content="Next.js Starter - Forty" />
           <link href="/css/skel.css" rel="stylesheet" />
           <link
